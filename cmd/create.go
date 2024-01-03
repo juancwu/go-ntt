499f6baeb5c9b6157a2b0760b7f9cd882ed7c5e9
jc <46619361+juancwu@users.noreply.github.com>
2024-01-03 10:18:11 -0500

    complete create migrations command

--- cmd/create.go
+++ cmd/create.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
 	"time"
 
+	"github.com/juancwu/go-ntt/util"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +17,7 @@ func create() *cobra.Command {
 		Short:   "Create migration files",
 		Long:    "Creates a pair of migration files for up/down migrations. You can select to use sequential (max of 6 digits) or timestamp format. Defaults to timestamp.",
 		Example: "ntt create NAME",
-		Aliases: []string{"c", "new"},
+		Aliases: []string{"new"},
 		Args:    cobra.ExactArgs(1),
 		RunE:    handle,
 	}
@@ -21,9 +26,35 @@ func create() *cobra.Command {
 }
 
 func handle(cmd *cobra.Command, args []string) error {
-	_, err := time.LoadLocation(timezone)
+	tz, err := time.LoadLocation(timezone)
 	if err != nil {
 		return err
 	}
+
+	timestamp := time.Now().In(tz)
+
+	name := args[0]
+
+	version := strconv.FormatInt(timestamp.Unix(), 10)
+
+	if err = os.MkdirAll(config.Source, os.ModePerm); err != nil {
+		return err
+	}
+
+	for _, dir := range [2]string{"up", "down"} {
+		filename := filepath.Join(config.Source, fmt.Sprintf("%s_%s_%s%s", version, name, dir, config.Ext))
+		util.Log().Debug("New migration file", "filename", filename)
+
+		file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+		if err != nil {
+			return err
+		}
+
+		err = file.Close()
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
